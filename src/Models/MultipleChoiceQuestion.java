0d46d3fb468fa47e81d9b13fb431c81b02cddfe7
LuuTienPhat <luutienphat@gmail.com>
2021-09-29 20:08:34 +0700

    Create class MultipleChoiceQuestion

---
+++ src/Models/MultipleChoiceQuestion.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Phat
+ */
+public class MultipleChoiceQuestion {
+
+    private String question;
+    private ArrayList<String> choices;
+    private String answer;
+
+    public MultipleChoiceQuestion() {
+    }
+
+    public MultipleChoiceQuestion(String question, ArrayList<String> choices, String answer) {
+        this.question = question;
+        this.choices = choices;
+        this.answer = answer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public ArrayList<String> getChoices() {
+        return choices;
+    }
+
+    public void setChoices(ArrayList<String> choices) {
+        this.choices = choices;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+}
