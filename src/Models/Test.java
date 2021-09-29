974df45502095cb7bdd2afc94a9afbf0184ce2fe
LuuTienPhat <luutienphat@gmail.com>
2021-09-29 20:37:52 +0700

    Replace: variable QA with multipleChoiceQuestion. Add: level to class Constructor and Get/Set change follow data members

---
+++ src/Models/Test.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+
+/**
+ *
+ * @author Phat
+ */
+public class Test {
+
+    private String StudentId;
+    private int score;
+    private MultipleChoiceQuestion multipleChoiceQuestion;
+    private String level;
+    private LocalDateTime datetime;
+
+    public Test() {
+    }
+
+    public Test(String StudentId, int score, MultipleChoiceQuestion multipleChoiceQuestion, String level, LocalDateTime datetime) {
+        this.StudentId = StudentId;
+        this.score = score;
+        this.multipleChoiceQuestion = multipleChoiceQuestion;
+        this.level = level;
+        this.datetime = datetime;
+    }
+
+    public String getStudentId() {
+        return StudentId;
+    }
+
+    public void setStudentId(String StudentId) {
+        this.StudentId = StudentId;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public MultipleChoiceQuestion getMultipleChoiceQuestion() {
+        return multipleChoiceQuestion;
+    }
+
+    public void setMultipleChoiceQuestion(MultipleChoiceQuestion multipleChoiceQuestion) {
+        this.multipleChoiceQuestion = multipleChoiceQuestion;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public LocalDateTime getDatetime() {
+        return datetime;
+    }
+
+    public void setDatetime(LocalDateTime datetime) {
+        this.datetime = datetime;
+    }
+
+}
